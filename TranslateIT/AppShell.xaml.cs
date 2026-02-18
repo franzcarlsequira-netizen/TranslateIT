8e63d56d2a67234d147a6fc34bb2e1b558f8ca72
Dx2s <franzcarlsequira@gmail.com>
2026-02-18 18:46:59 +0800

    Add files via upload

---
+++ TranslateIT/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TranslateIT
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TranslateIT/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TranslateIT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
